fc1b3c6ed07a08c05a5b16a2b18a47f0ae673c85
mabrams <mabrams@uab.edu>
2015-05-01 11:51:41 -0500

    temporary change to run MWE Bar class

--- 1/students/project.clj
+++ 1/students/project.clj
@@ -10,13 +10,14 @@
   ;:aot [students.Student]
   ;:main students.core ; 'lein run' will run students/core.clj; 'lein repl' won't
   ;:main ^:skip-aot students.core  ; I don't think I need or want skip-aot
-  :main students.StudentsWithUI ; run the Java code directly without calling it from Clojure
+  ;:main students.StudentsWithUI ; run the Java code directly without calling it from Clojure
+  :main students.Bar ; run the Java code directly without calling it from Clojure
   ;:target-path "target/%s"
   ;:profiles {:uberjar {:aot :all}}
 
   ;; See https://github.com/technomancy/leiningen/blob/master/doc/MIXED_PROJECTS.md
   :profiles {:precomp {:source-paths ["src/pre/clojure"]
-                        :aot [students.Student] } 
+                        :aot [students.Student students.Foo] } 
              :midcomp {:source-paths ["src/clojure"]
                        :java-source-paths ["src/java"]
                        :aot [students.core] }} 
