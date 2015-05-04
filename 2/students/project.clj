75bc4ec2e20abaa849348ea793f1888c37298356
mars0i <mabrams@uab.edu>
2015-05-03 22:26:07 -0500

    Cleaned up, added separate withgui and nogui profiles

--- 2/students/project.clj
+++ 2/students/project.clj
@@ -2,22 +2,16 @@
   :description "Environment for experiments with integrating Mason and Clojure"
   :license {:name "Gnu General Public License version 3.0"
             :url "http://www.gnu.org/copyleft/gpl.html"}
-  ;:source-paths ["src/cloljure"]
-  ;:java-source-paths ["src/java"]
   :resource-paths ["resources/mason.18.jar"]
   :dependencies [[org.clojure/clojure "1.6.0"]]
-  ;:aot :all
-  ;:aot [students.Student]
-  ;:main students.core ; 'lein run' will run students/core.clj; 'lein repl' won't
-  ;:main ^:skip-aot students.core  ; I don't think I need or want skip-aot
-  :main students.StudentsWithUI ; run the Java code directly without calling it from Clojure
-  ;:target-path "target/%s"
-  ;:profiles {:uberjar {:aot :all}}
+  :main students.StudentsWithUI ; default action with 'lein run'
 
   ;; See https://github.com/technomancy/leiningen/blob/master/doc/MIXED_PROJECTS.md
-  :profiles {:comp1 {:source-paths ["src/pre/clojure"]
+  :profiles {:comp1 {:source-paths ["src/pre/clojure"]               ; see compile.sh
                         :aot [students.Student students.Students] } 
-             :comp2 {:source-paths ["src/clojure"]
+             :comp2 {:source-paths ["src/clojure"]                   ; see compile.sh
                        :java-source-paths ["src/java"]
-                       :aot [students.core] }} 
-)
+                       :aot [students.core] }
+
+             :withgui {:main students.StudentsWithUI} ; execute this with 'lein with-profile withgui run'
+             :nogui   {:main students.Students} } )   ; execute this with 'lein with-profile nogui run'
