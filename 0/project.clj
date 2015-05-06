3af157e2b6809d8ed403261b3a849b5915a2e946
mabrams <mabrams@uab.edu>
2015-05-05 22:49:25 -0500

    Added Java-only version.

---
+++ 0/project.clj
@@ -0,0 +1,24 @@
+(defproject students "0.1.0-SNAPSHOT"
+  :description "Environment for experiments with integrating Mason and Clojure"
+  :license {:name "Gnu General Public License version 3.0"
+            :url "http://www.gnu.org/copyleft/gpl.html"}
+  ;:source-paths ["src/cloljure"]
+  ;:java-source-paths ["src/java"]
+  :resource-paths ["resources/mason.18.jar"
+                   "resources/hamcrest-core-1.3.jar"
+                   "resources/jcommon-1.0.23.jar"
+                   "resources/jfreechart-1.0.19-experimental.jar"
+                   "resources/jfreechart-1.0.19-swt.jar"
+                   "resources/jfreechart-1.0.19.jar"
+                   "resources/jfreesvg-2.0.jar"
+                   "resources/junit-4.11.jar"
+                   "resources/orsoncharts-1.4-eval-nofx.jar"
+                   "resources/orsonpdf-1.6-eval.jar"
+                   "resources/servlet.jar"
+                   "resources/swtgraphics2d.jar"]
+  :dependencies [[org.clojure/clojure "1.6.0"]]
+  :main students.StudentsWithUI
+  :source-paths ["src/clojure"]
+  :java-source-paths ["src/java"]
+)
+
