785f93414ed4dbbed7a0eccaf0344e2524b1407e
mars0i <mabrams@uab.edu>
2015-05-18 22:28:11 -0500

    Argument for cyclic dependencies.

--- 3plus/project.clj
+++ 3plus/project.clj
@@ -10,7 +10,8 @@
                    "resources/jmf.jar"
                    "resources/mason.18.jar"
                    "resources/portfolio.jar"]
-  :dependencies [[org.clojure/clojure "1.6.0"]]
+  ;:dependencies [[org.clojure/clojure "1.6.0"]]
+  :dependencies [[org.clojure/clojure "1.7.0-beta3"]]
   :main students.StudentsWithUI ; default action with 'lein run'
   :source-paths ["src/clojure"]
   :aot [students.Student students.Students students.StudentsWithUI]
