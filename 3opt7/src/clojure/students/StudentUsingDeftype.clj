de6661c0b7b0341e8615b51ee9f4ef6bdea3e098
mars0i <mabrams@uab.edu>
2015-05-21 10:28:13 -0500

    updated from old versions forgot to copy Student.clj

--- 3opt7/src/clojure/students/StudentUsingDeftype.clj
+++ 3opt7/src/clojure/students/StudentUsingDeftype.clj
@@ -27,7 +27,7 @@
             [getAgitation [] double]])
 (import [students SteppableStudent])
 
-(deftype Student [student-state] students.SteppableStudent
+(deftype Student [agitation] students.SteppableStudent
   (step [this students]
     ;; Note that this code is functional until the last step.
     (let [^AltState alt-state (.gitAltState students)
@@ -46,17 +46,16 @@
                                                 (.-height yard)
                                                 alt-state))
           ;; buddy forces from attraction and repulsion to/from other students:
-          [buddy-force-x buddy-force-y agitation] (collect-buddy-forces alt-state this)]  ; 'Go through my buddies and determine how much I want to be near them' (for-loop, p. 27 middle)
+          [buddy-force-x buddy-force-y agit] (collect-buddy-forces alt-state this)]  ; 'Go through my buddies and determine how much I want to be near them' (for-loop, p. 27 middle)
       ;; Now finish with all of the imperative code in one place:
-      (reset! (:agitation student-state) agitation)   ; (p. 31: friendsClose, enemiesCloser)
+      (println (:agitation this) agit)   ; (p. 31: friendsClose, enemiesCloser)
       (.setObjectLocation yard this    ; modify location for me in yard in students (end of step(), p. 18 top, p. 27 bottom):
                           (Double2D. (+ curr-x indiv-force-x buddy-force-x)
                                      (+ curr-y indiv-force-y buddy-force-y)))))
   (toString [this] (str "[" (System/identityHashCode this) "] agitation: " (.getAgitation this)))
-  (getAgitation [this] @(:agitation student-state)))
+  (getAgitation [this] @(:agitation this)))
 
-(defn make-student []
-  (Student. {:agitation (atom 0.0)}))
+(defn make-student [] (Student. (atom 0.0)))
 
 
 (defn ^double wander-force-coord
