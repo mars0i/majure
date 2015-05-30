7507194c9434a26be9e89575cf941f9a0704f209
mabrams <mabrams@uab.edu>
2015-05-29 19:21:16 -0500

    exploring type hints and defprotocol

--- 3opt16/src/clojure/utils/typeabbrevs.clj
+++ 3opt16/src/clojure/utils/typeabbrevs.clj
@@ -6,6 +6,13 @@
 ;; that will be required in order to have those methods.  These macros
 ;; will automatically define a protocol or interface for this purpose.
 
+
+;; This has no role for type hints, etc.  Uh oh.
+;; One option would be to use only protocols, and then have type hints required,
+;; but then only used them in the protocol def (which you can do).
+;; However, in that case, to keep the macro def simple, we should require that there always
+;; be a type hint on return value, even if it's ^void.
+
 (defmacro defrec-with-prot
   "Define a protocol matching the methods in meths, and then define
   a record type using recname, fields, and that protocol."
